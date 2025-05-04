575c5b18ea890f1c63b53b47f4fcd7cb9a423e21
Eugene233 <agboreugene@gmail.com>
2025-05-04 13:44:22 +0200

    Added user get-post

--- api.py
+++ api.py
@@ -1,19 +1,53 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import (Resource, Api, reqparse,
+                           fields, marshal_with, abort)
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-
 db = SQLAlchemy(app)
+api = Api(app)
 
-class User(db.Model):
+
+class UserModel(db.Model):
     id  = db.Column(db.Integer, primary_key=True)
     name  = db.Column(db.String(80), unique=True, nullable=False)
     email  = db.Column(db.String(80), unique=True, nullable=False)
 
     def __repr__(self):
         return f"User(name= {self.name}, email= {self.email})"
+
+
+# Used for validateion
+user_args = reqparse.RequestParser()
+user_args.add_argument('name', type=str, help="Please enter a name")
+user_args.add_argument('email', type=str, help="Please enter an Email")
+
+# Used for serialization
+userFields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'email': fields.String
+}
+
+class Users(Resource):
+    @marshal_with(userFields)
+    def get(self):
+        users = UserModel.query.all()
+        return users
+
+    @marshal_with(userFields)
+    def post(self):
+        args = user_args.parse_args()
+        user = UserModel(name=args['name'], email=args['email'])
+        db.session.add(user)
+        db.session.commit()
+        users = UserModel.query.all()
+        return users, 201
+
+
+api.add_resource(Users, '/api/users/')
 
 
 @app.route('/')
